1e224309bdfc63c2e6cc007e1d1d5ae447cfc743
Nicola Scendoni <nscendoni@adobe.com>
2026-03-31 12:53:28 +0200

    added ConfigurationPolicy.REQUIRE to JcrUserHomeOAuthTokenStore

--- src/main/java/org/apache/sling/auth/oauth_client/impl/JcrUserHomeOAuthTokenStore.java
+++ src/main/java/org/apache/sling/auth/oauth_client/impl/JcrUserHomeOAuthTokenStore.java
@@ -36,11 +36,12 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Component
+@Component(configurationPolicy = ConfigurationPolicy.REQUIRE)
 public class JcrUserHomeOAuthTokenStore implements OAuthTokenStore {
 
     private static final String PROPERTY_NAME_EXPIRES_AT = "expires_at";
