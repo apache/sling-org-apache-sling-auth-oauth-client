0aec2552ecad6be13312342b00eaeaf082d61219
Nicola Scendoni <nscendoni@adobe.com>
2026-03-31 14:24:36 +0200

    Restored JcrUserHomeOAuthTokenStore(@Reference CryptoService cryptoService) as in master

--- src/main/java/org/apache/sling/auth/oauth_client/impl/JcrUserHomeOAuthTokenStore.java
+++ src/main/java/org/apache/sling/auth/oauth_client/impl/JcrUserHomeOAuthTokenStore.java
@@ -36,12 +36,14 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Component(configurationPolicy = ConfigurationPolicy.REQUIRE)
+import static org.osgi.service.component.annotations.ConfigurationPolicy.REQUIRE;
+
+// a config class is intentionally not defined, but a config is required to select an implementation
+@Component(configurationPolicy = REQUIRE)
 public class JcrUserHomeOAuthTokenStore implements OAuthTokenStore {
 
     private static final String PROPERTY_NAME_EXPIRES_AT = "expires_at";
